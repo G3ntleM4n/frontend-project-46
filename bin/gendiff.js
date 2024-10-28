40c810ed3210e998e19edef7e55bcef6616218d0
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-10-28 17:55:36 +0300

    add ability to display reference information

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log(1 + 2);
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
+
+program.parse();
