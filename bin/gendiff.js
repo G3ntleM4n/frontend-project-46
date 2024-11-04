362a0efd1fcae6aea7332935dcabbbfe043c2194
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-11-04 20:17:50 +0300

    add arguments and option to CLI command

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
