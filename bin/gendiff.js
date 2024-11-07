6442d8312f028597b475f4a047db128b0292ef98
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-11-07 17:53:00 +0300

    add action to the program

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import parserJson from '../src/parser.js';
 
 const program = new Command();
 
@@ -8,8 +9,9 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
   .argument('<filepath1>', 'path to first file')
   .argument('<filepath2>', 'path to second file')
-  .option('-f, --format [type]', 'output format');
+  .action((filepath1, filepath2) => parserJson(filepath1, filepath2));
 
 program.parse();
