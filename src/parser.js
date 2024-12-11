2446b291868689a2b98e7948da6f7f2a55b373a8
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-12-11 18:17:24 +0300

    add comments to parser.js

--- src/parser.js
+++ src/parser.js
@@ -3,7 +3,8 @@ import * as fs from 'node:fs';
 const parserJson = (filepath1, filepath2) => {
   const file1 = fs.readFileSync(filepath1, { encoding: 'utf-8' });
   const file2 = fs.readFileSync(filepath2, { encoding: 'utf-8' });
-  console.log(JSON.parse(file1));
-  console.log(JSON.parse(file2));
+  // console.log(JSON.parse(file1));
+  // console.log(JSON.parse(file2));
+  return [JSON.parse(file1), JSON.parse(file2)];
 };
 export default parserJson;
