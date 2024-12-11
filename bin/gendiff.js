66d446cfe84d7ad50abc17622eb9868e2117adda
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-12-11 18:17:55 +0300

    change action file in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 
 import { Command } from 'commander';
 import parserJson from '../src/parser.js';
+import findDiff from '../src/findDiff.js';
 
 const program = new Command();
 
@@ -12,6 +13,7 @@ program
   .option('-f, --format [type]', 'output format')
   .argument('<filepath1>', 'path to first file')
   .argument('<filepath2>', 'path to second file')
-  .action((filepath1, filepath2) => parserJson(filepath1, filepath2));
+  .action((filepath1, filepath2) => parserJson(filepath1, filepath2))
+  .action((filepath1, filepath2) => findDiff(filepath1, filepath2));
 
 program.parse();
