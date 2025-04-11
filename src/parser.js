841d60d2897ccb93317f542603c9c4878338bba8
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-04-11 17:30:29 +0300

    delete excess comments

--- src/parser.js
+++ src/parser.js
@@ -4,8 +4,6 @@ const parserJson = (filepath1, filepath2) => {
   try {
     const file1 = fs.readFileSync(filepath1, { encoding: 'utf-8' });
     const file2 = fs.readFileSync(filepath2, { encoding: 'utf-8' });
-    // console.log(JSON.parse(file1));
-    // console.log(JSON.parse(file2));
     return [JSON.parse(file1), JSON.parse(file2)];
   } catch (error) {
     console.log('\nERROR: one of the files can\'t be parsed\n');
