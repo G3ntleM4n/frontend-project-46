00ee3ac1baf1be8262d3d743331532ce83c33e9c
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-04-11 16:15:31 +0300

    add try catch statement at src/parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,15 @@
 import * as fs from 'node:fs';
 
 const parserJson = (filepath1, filepath2) => {
-  const file1 = fs.readFileSync(filepath1, { encoding: 'utf-8' });
-  const file2 = fs.readFileSync(filepath2, { encoding: 'utf-8' });
-  // console.log(JSON.parse(file1));
-  // console.log(JSON.parse(file2));
-  return [JSON.parse(file1), JSON.parse(file2)];
+  try {
+    const file1 = fs.readFileSync(filepath1, { encoding: 'utf-8' });
+    const file2 = fs.readFileSync(filepath2, { encoding: 'utf-8' });
+    // console.log(JSON.parse(file1));
+    // console.log(JSON.parse(file2));
+    return [JSON.parse(file1), JSON.parse(file2)];
+  } catch (error) {
+    console.log('\nERROR: one of the files can\'t be parsed\n');
+    return 0;
+  }
 };
 export default parserJson;
