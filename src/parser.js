d8465b63070563e52148509625a23476ce577958
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-05-15 14:27:56 +0300

    remove try catch statement

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,8 @@
 import * as fs from 'node:fs';
 
 const parserJson = (filepath1, filepath2) => {
-  try {
-    const file1 = fs.readFileSync(filepath1, { encoding: 'utf-8' });
-    const file2 = fs.readFileSync(filepath2, { encoding: 'utf-8' });
-    return [JSON.parse(file1), JSON.parse(file2)];
-  } catch (error) {
-    console.log('\nERROR: one of the files can\'t be parsed\n');
-    return 0;
-  }
+  const file1 = fs.readFileSync(filepath1, { encoding: 'utf-8' });
+  const file2 = fs.readFileSync(filepath2, { encoding: 'utf-8' });
+  return [JSON.parse(file1), JSON.parse(file2)];
 };
 export default parserJson;
