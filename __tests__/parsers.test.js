37fcc70069ab8164d5b5552704073884e1d5b1a3
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-05-25 17:00:06 +0300

    rename and add tests for yaml comparison

--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -1,28 +1,29 @@
 import path from 'node:path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import parserJson from '../src/parser.js';
+import { parserJson, parserYml } from '../src/parsers.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-let parsedJson1;
-let parsedJson2;
+let parsedFirst;
+let parsedSecond;
 let filepath1;
 let filepath2;
 let filepath3;
+let filepath4;
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 beforeEach(() => {
-  parsedJson1 = {
+  parsedFirst = {
     host: 'hexlet.io',
     timeout: 50,
     proxy: '123.234.53.22',
     follow: false,
   };
 
-  parsedJson2 = {
+  parsedSecond = {
     timeout: 20,
     verbose: true,
     host: 'hexlet.io',
@@ -30,12 +31,22 @@ beforeEach(() => {
 
   filepath1 = getFixturePath('testFile1.json');
   filepath2 = getFixturePath('testFile2.json');
-  filepath3 = getFixturePath('testFile3.json');
+  filepath3 = getFixturePath('testYaml1.yml');
+  filepath4 = getFixturePath('testYaml2.yaml');
 });
 
-test('parserTest', () => {
-  expect(parserJson(filepath1, filepath2)).toEqual([parsedJson1, parsedJson2]);
+test('parserJsonTest', () => {
+  expect(parserJson(filepath1)).toEqual(parsedFirst);
+  expect(parserJson(filepath2)).toEqual(parsedSecond);
   expect(() => {
-    parserJson(filepath1, filepath3);
+    parserJson(filepath3);
+  }).toThrow();
+});
+
+test('parserYmlTest', () => {
+  expect(parserYml(filepath3)).toEqual(parsedFirst);
+  expect(parserYml(filepath4)).toEqual(parsedSecond);
+  expect(() => {
+    parserYml(filepath1);
   }).toThrow();
 });
