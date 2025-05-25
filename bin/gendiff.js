92dbec228bfcbb4cfdfd402d1999a1a62a775449
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-05-25 17:03:54 +0300

    refactor and add yaml comparison

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import parserJson from '../src/parser.js';
+import { parserJson, parserYml } from '../src/parsers.js';
 import findDiff from '../src/flatCompare.js';
 
 const program = new Command();
@@ -14,7 +14,19 @@ program
   .argument('<filepath1>', 'path to the first file')
   .argument('<filepath2>', 'path to the second file')
   .action((filepath1, filepath2) => {
-    const [data1, data2] = parserJson(filepath1, filepath2);
+    let data1;
+    let data2;
+    if (filepath1.includes('.json')) {
+      data1 = parserJson(filepath1);
+    } else {
+      data1 = parserYml(filepath1);
+    }
+
+    if (filepath2.includes('.json')) {
+      data2 = parserJson(filepath2);
+    } else {
+      data2 = parserYml(filepath2);
+    }
     console.log(findDiff(data1, data2));
   });
 
