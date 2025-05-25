234d1a2ec275147fc8a62dd758afd844ea67011b
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-05-25 17:24:47 +0300

    refactor action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import path from 'node:path';
 import { Command } from 'commander';
 import { parserJson, parserYml } from '../src/parsers.js';
 import findDiff from '../src/flatCompare.js';
@@ -16,13 +17,16 @@ program
   .action((filepath1, filepath2) => {
     let data1;
     let data2;
-    if (filepath1.includes('.json')) {
+    const format1 = path.extname(filepath1);
+    const format2 = path.extname(filepath2);
+
+    if (format1 === '.json') {
       data1 = parserJson(filepath1);
     } else {
       data1 = parserYml(filepath1);
     }
 
-    if (filepath2.includes('.json')) {
+    if (format2 === '.json') {
       data2 = parserJson(filepath2);
     } else {
       data2 = parserYml(filepath2);
