6f98f4aace084e0728d7c92c02ea9bedebebbf52
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-11-07 16:14:44 +0300

    fix flat files compare

--- src/flatCompare.js
+++ src/flatCompare.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 
-const findDiff = (data1, data2) => {
+const findDiffFlat = (data1, data2) => {
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+  console.log(JSON.stringify(keys, null, 2));
 
   const diff = {};
   keys.map((key) => {
@@ -13,11 +14,14 @@ const findDiff = (data1, data2) => {
       diff[`- ${key}`] = data1[key];
       diff[`+ ${key}`] = data2[key];
     } else {
-      diff[`  ${key}`] = data2[key];
+      diff[`  ${key}`] = data1[key];
     }
+
     return diff;
   });
+
+  console.log(diff);
   return diff;
 };
 
-export default findDiff;
+export default findDiffFlat;
