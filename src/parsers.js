544b6b942bf5c3dd6b57bc2b90dbb9da77adfaf1
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-11-07 16:15:03 +0300

    fix parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,18 +1,22 @@
 import * as fs from 'node:fs';
+import path from 'node:path';
+import { cwd } from 'node:process';
 import yaml from 'js-yaml';
+
+const pathResolve = (filepath) => path.resolve(cwd(), filepath);
 
 export const parserJson = (filepath) => {
   if (!filepath.includes('.json')) {
-    throw new Error("file extension isn't right");
+    throw new Error("file extension isn't JSON");
   }
-  const file = fs.readFileSync(filepath, { encoding: 'utf-8' });
+  const file = fs.readFileSync(pathResolve(filepath), { encoding: 'utf-8' });
   return JSON.parse(file);
 };
 
 export const parserYml = (filepath) => {
   if (!filepath.includes('.yml') && !filepath.includes('.yaml')) {
-    throw new Error("file extension isn't right");
+    throw new Error("file extension isn't YAML");
   }
-  const file = fs.readFileSync(filepath, { encoding: 'utf-8' });
+  const file = fs.readFileSync(pathResolve(filepath), { encoding: 'utf-8' });
   return yaml.load(file);
 };
