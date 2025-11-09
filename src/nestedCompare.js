bf1df2b580beba8a68b353808f39966b9286fb30
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-11-09 20:05:26 +0300

    fix nested compare function

--- src/nestedCompare.js
+++ src/nestedCompare.js
@@ -1,119 +1,36 @@
 import _ from 'lodash';
 
-const findKeys = (data) => {
-  const entries = Object.entries(data);
-  const keys = entries.map((entry) => {
-    const iter = ([key, value], acc) => {
-      acc.push(key);
-
-      if (!_.isObject(value)) {
-        return acc;
-      }
-
-      Object.entries(value).map((child) => iter(child, acc));
-
-      return acc;
-    };
-    return iter(entry, []);
-  });
-
-  return keys;
-};
-
-const pathJoin = (path, toJoin) => {
-  const joinedPath = `${path}.${toJoin}`;
-  return joinedPath;
-};
-
 const findDiffNested = (data1, data2) => {
-  const keys = _.union(findKeys(data1), findKeys(data2));
-  const firstKeys = _.union(Object.keys(data1), Object.keys(data2));
-  const allData = _.merge(_.cloneDeep(data1), _.cloneDeep(data2));
-  console.log(allData);
-  console.log('\n\n\n');
+  const iter = (d1, d2) => {
+    const diff = {};
+    const keys = _.sortBy(_.union(Object.keys(d1), Object.keys(d2)));
 
-  const diff = {};
-  const diff2 = {};
+    /* console.log('keys: ', keys);
+    console.log('\n\n\n'); */
 
-  firstKeys.map((key) => {
-    const iter = (entryKey, path = entryKey) => {
-      const value1 = _.get(data1, path);
-      const value2 = _.get(data2, path);
-      const allValue = _.get(allData, path);
-      /* console.log(path);
-      console.log(allValue);
-      console.log('\n\n\n'); */
-
-      if (!_.isObject(value1) || !_.isObject(value2)) {
-        // Если значение ключа - объект и при этом у одного из файлов не было
-        // этого ключа, то просто добавляем ключ со значением и не углубляемся
-        if (_.isObject(value2)) {
-          diff[`+ ${entryKey}`] = value2;
-        } else if (_.isObject(value1)) {
-          diff[`- ${entryKey}`] = value1;
-        }
-        if (!Object.hasOwn(data1, key)) {
-          diff[`+ ${entryKey}`] = value2;
-        } else if (!Object.hasOwn(data2, key)) {
-          diff[`- ${entryKey}`] = value1;
-        } else if (value1 !== value2) {
-          diff[`- ${entryKey}`] = value1;
-          diff[`+ ${entryKey}`] = value2;
+    keys.map((key) => {
+      if (!Object.hasOwn(d1, key)) {
+        diff[`+ ${key}`] = d2[key];
+      } else if (!Object.hasOwn(d2, key)) {
+        diff[`- ${key}`] = d1[key];
+      } else if (d1[key] !== d2[key]) {
+        if (typeof d1[key] === 'object' && typeof d2[key] === 'object') {
+          diff[`  ${key}`] = iter(d1[key], d2[key]);
         } else {
-          diff[`  ${entryKey}`] = value1;
+          diff[`- ${key}`] = d1[key];
+          diff[`+ ${key}`] = d2[key];
         }
-      } else if (_.isObject(value1) && _.isObject(value2)) {
-        diff[`  ${entryKey}`] = '';
-        Object.keys(allValue).map((keyN) => iter(keyN, pathJoin(path, keyN)));
-        // Object.keys(diff2).forEach((keyDiff2) => delete diff2[keyDiff2]);
+      } else {
+        diff[`  ${key}`] = d1[key];
       }
-
-      // return diff;
-    };
-
-    iter(key);
-  });
-
-  const finDiff = {};
-
-  const makeFinDiff = (object) => Object.entries(object).map(([key, value]) => {
-    if (value !== undefined) {
-      finDiff[key] = value;
-    }
-  });
-
-  makeFinDiff(diff);
-
-  console.log(finDiff);
-  return finDiff;
-
-  /*
-  const iter = (node1, node2, depth) => {
-    // console.log(depth);
-    const [key1, value1] = Object.entries(node1)[depth];
-    const [key2, value2] = Object.entries(node2)[depth];
-
-    if (!Object.hasOwn(node1, key2)) {
-      diff[`+ ${key2}`] = value2;
-    } else if (!Object.hasOwn(node2, key1)) {
-      diff[`- ${key1}`] = value1;
-    } else if (value1 !== value2) {
-      diff[`- ${key1}`] = value1;
-      diff[`+ ${key2}`] = value2;
-    } else {
-      diff[`  ${key2}`] = value2;
-    }
-
-    if (_.isObject(value1) && _.isObject(value2)) {
-      iter(value1, value2, depth + 1);
-    }
-
-    // console.log(diff);
+      // console.log('this level diff: ', diff);
+      return diff;
+    });
     return diff;
   };
-
-  // console.log(diff);
-  return iter(data1, data2, 0);
-  */
+  const globalDiff = iter(data1, data2);
+  console.log(globalDiff);
+  return globalDiff;
 };
+
 export default findDiffNested;
