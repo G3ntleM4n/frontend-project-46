bc8cde385d1a3bac920586b5b4898c1a095dc6e4
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-11-14 16:06:44 +0300

    fix stylish output

--- src/outputStyles.js
+++ src/outputStyles.js
@@ -1,54 +1,36 @@
-/* import _ from 'lodash';
-
-const styleArray = (array) => `{${array.join('')}\n}`; */
-
-/* const stylishOutput = (target, replacer = ' ', spacesCount = 4) => {
-  const arrayOfEntries = Object.entries(target).map((prop) => {
-    const iter = (obj, acc, depth = 1) => {
-      const [key, value] = obj;
-      let string = acc;
-
-      if (!_.isObject(value)) {
-        if (key.startsWith('-') || key.startsWith('+') || key.startsWith(' ')) {
-          string += `\n${replacer.repeat(spacesCount * depth - 2)}${key}: ${value}`;
-        } else {
-          string += `\n${replacer.repeat(spacesCount * depth)}${key}: ${value}`;
-        }
-      }
-
-      if (_.isObject(value)) {
-        const nest = Object.entries(value).map((entry) => iter(entry, '', depth + 1));
-        if (key.startsWith('-') || key.startsWith('+') || key.startsWith(' ')) {
-          string = `\n${replacer.repeat(spacesCount * depth - 2)}${key}: {${nest.join('')}`;
-        } else {
-          string = `\n${replacer.repeat(spacesCount * depth)}${key}: {${nest.join('')}`;
-        }
-        string += `\n${replacer.repeat(spacesCount * depth)}}`;
-      }
-
-      return string;
-    };
-
-    return iter(prop, '');
-  });
-
-  const result = styleArray(arrayOfEntries);
-
-  console.log(result);
-  return result;
-}; */
-
 const stylishOutput = (object, spacesCount = 2, prefix = ' ') => {
   let result = '{\n';
-  Object.entries(object).forEach((entry) => {
-    const iter = ([key, value], depth = 1) => {
-      result += `${prefix.repeat(spacesCount * depth)}${key}: ${value}\n`;
-    };
+  const iter = (node, depth) => {
+    let diff = '';
+    Object.entries(node).forEach(([key, value]) => {
+      if (key.search(/^(\+|-| )/) !== -1) { // if key starts with '+' or '-' or space
+        if (typeof value === 'object' && value !== null) {
+          const dive = iter(value, depth + 2);
+          diff += `${prefix.repeat(spacesCount * depth)}${key}: {\n`;
+          diff += dive;
+          diff += `${prefix.repeat(spacesCount * depth)}  }\n`;
+        } else {
+          diff += `${prefix.repeat(spacesCount * depth)}${key}: ${value}\n`;
+        }
+        return diff;
+      }
+      if (typeof value === 'object' && value !== null) {
+        const dive = iter(value, depth + 2);
+        diff += `${prefix.repeat(spacesCount * depth)}  ${key}: {\n`;
+        diff += dive;
+        diff += `${prefix.repeat(spacesCount * depth)}  }\n`;
+      } else {
+        diff += `${prefix.repeat(spacesCount * depth)}  ${key}: ${value}\n`;
+      }
+      return diff;
+    });
 
-    iter(entry);
-  });
+    return diff;
+  };
+
+  result += iter(object, 1);
   result += '}';
-  console.log(result);
+  // console.log(result);
   return result;
 };
 
