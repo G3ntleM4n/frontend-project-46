be64032e37a47d4fe48e5c18517b8e1f25f4c1b5
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-11-20 20:20:29 +0300

    add capability of changing formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,7 @@
 import path from 'node:path';
 import { Command } from 'commander';
 import { parserJson, parserYml } from '../src/parsers.js';
-import stylishOutput from '../src/outputStyles.js';
+import { stylishOutput, plainOutput } from '../src/outputStyles.js';
 import findDiffNested from '../src/nestedCompare.js';
 
 const program = new Command();
@@ -12,12 +12,14 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<filepath1>', 'path to the first file')
   .argument('<filepath2>', 'path to the second file')
   .action((filepath1, filepath2) => {
     let data1;
     let data2;
+    let result;
+    const formatter = program.opts().format;
     const format1 = path.extname(filepath1);
     const format2 = path.extname(filepath2);
 
@@ -32,7 +34,14 @@ program
     } else {
       data2 = parserYml(filepath2);
     }
-    const result = stylishOutput(findDiffNested(data1, data2));
+    switch (formatter) {
+      case 'plain':
+        result = plainOutput(findDiffNested(data1, data2));
+        break;
+      default:
+        result = stylishOutput(findDiffNested(data1, data2));
+        break;
+    }
     console.log(result);
     return result;
   });
