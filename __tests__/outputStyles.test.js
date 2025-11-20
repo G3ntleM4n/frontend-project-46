0cb71fd77f271e2eb053474ab356f006cc93c2e0
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-11-20 20:17:41 +0300

    add test for plain formatter

--- __tests__/outputStyles.test.js
+++ __tests__/outputStyles.test.js
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import stylishOutput from '../src/outputStyles.js';
+import { stylishOutput, plainOutput } from '../src/outputStyles.js';
 import { parserJson, parserYml } from '../src/parsers.js';
 import findDiffFlat from '../src/flatCompare.js';
 import findDiffNested from '../src/nestedCompare.js';
@@ -21,6 +21,7 @@ let object7;
 let object8;
 let expected;
 let expectedNested;
+let expectedNestedPlain;
 let diffFlatJson;
 let diffFlatYaml;
 let diffNestedJson;
@@ -44,6 +45,18 @@ beforeAll(() => {
   expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
 
   expectedNested = '{\n    common: {\n      + follow: false\n        setting1: Value 1\n      - setting2: 200\n      - setting3: true\n      + setting3: null\n      + setting4: blah blah\n      + setting5: {\n            key5: value5\n        }\n        setting6: {\n            doge: {\n              - wow: \n              + wow: so much\n            }\n            key: value\n          + ops: vops\n        }\n    }\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n  - group2: {\n        abc: 12345\n        deep: {\n            id: 45\n        }\n    }\n  + group3: {\n        deep: {\n            id: {\n                number: 45\n            }\n        }\n        fee: 100500\n    }\n}';
+
+  expectedNestedPlain = "Property 'common.follow' was added with value: false"
+    + "\nProperty 'common.setting2' was removed"
+    + "\nProperty 'common.setting3' was updated. From true to null"
+    + "\nProperty 'common.setting4' was added with value: 'blah blah'"
+    + "\nProperty 'common.setting5' was added with value: [complex value]"
+    + "\nProperty 'common.setting6.doge.wow' was updated. From '' to 'so much'"
+    + "\nProperty 'common.setting6.ops' was added with value: 'vops'"
+    + "\nProperty 'group1.baz' was updated. From 'bas' to 'bars'"
+    + "\nProperty 'group1.nest' was updated. From [complex value] to 'str'"
+    + "\nProperty 'group2' was removed"
+    + "\nProperty 'group3' was added with value: [complex value]";
 });
 
 test('stylish output test', () => {
@@ -52,4 +65,8 @@ test('stylish output test', () => {
 
   expect(stylishOutput(diffNestedJson)).toStrictEqual(expectedNested);
   expect(stylishOutput(diffNestedYaml)).toStrictEqual(expectedNested);
+});
+test('plain output test', () => {
+  expect(plainOutput(diffNestedJson)).toStrictEqual(expectedNestedPlain);
+  expect(plainOutput(diffNestedYaml)).toStrictEqual(expectedNestedPlain);
 });
