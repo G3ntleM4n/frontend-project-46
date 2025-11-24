154838ca50231a252ae3bf3dc12abb2e6ffa06e2
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-11-24 15:13:56 +0300

    refactor nestedCompare file

--- src/nestedCompare.js
+++ src/nestedCompare.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 
+const isObject = (value) => typeof value === 'object' && value !== null;
+
 const findDiffNested = (data1, data2) => {
   const iter = (d1, d2) => {
     const diff = {};
@@ -10,17 +12,14 @@ const findDiffNested = (data1, data2) => {
         diff[`+ ${key}`] = d2[key];
       } else if (!Object.hasOwn(d2, key)) {
         diff[`- ${key}`] = d1[key];
-      } else if (d1[key] !== d2[key]) {
-        if (typeof d1[key] === 'object' && typeof d2[key] === 'object') {
-          diff[`  ${key}`] = iter(d1[key], d2[key]);
-        } else {
-          diff[`- ${key}`] = d1[key];
-          diff[`+ ${key}`] = d2[key];
-        }
-      } else {
+      } else if (d1[key] === d2[key]) {
         diff[`  ${key}`] = d1[key];
+      } else if (isObject(d1[key]) && isObject(d2[key])) {
+        diff[`  ${key}`] = iter(d1[key], d2[key]);
+      } else {
+        diff[`- ${key}`] = d1[key];
+        diff[`+ ${key}`] = d2[key];
       }
-
       return diff;
     });
 
