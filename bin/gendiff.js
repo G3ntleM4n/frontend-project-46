7da9c49ef299834555bc165c9608a1be49b02933
Sokolov Alexander <alexsokolov123654@mail.ru>
2025-11-26 15:54:29 +0300

    refactor executable by moving logic to src/genDiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
-import path from 'node:path';
 import { Command } from 'commander';
-import { parserJson, parserYml } from '../src/parsers.js';
-import { stylishOutput, plainOutput, jsonOutput } from '../src/outputStyles.js';
-import findDiffNested from '../src/nestedCompare.js';
+
+import genDiff from '../src/genDiff.js';
 
 const program = new Command();
 
@@ -16,37 +14,8 @@ program
   .argument('<filepath1>', 'path to the first file')
   .argument('<filepath2>', 'path to the second file')
   .action((filepath1, filepath2) => {
-    let data1;
-    let data2;
-    let result;
     const formatter = program.opts().format;
-    const format1 = path.extname(filepath1);
-    const format2 = path.extname(filepath2);
-
-    if (format1 === '.json') {
-      data1 = parserJson(filepath1);
-    } else {
-      data1 = parserYml(filepath1);
-    }
-
-    if (format2 === '.json') {
-      data2 = parserJson(filepath2);
-    } else {
-      data2 = parserYml(filepath2);
-    }
-    switch (formatter) {
-      case 'plain':
-        result = plainOutput(findDiffNested(data1, data2));
-        break;
-      case 'json':
-        result = jsonOutput(findDiffNested(data1, data2));
-        break;
-      default:
-        result = stylishOutput(findDiffNested(data1, data2));
-        break;
-    }
-    console.log(result);
-    return result;
+    return genDiff(formatter, filepath1, filepath2);
   });
 
 program.parse();
